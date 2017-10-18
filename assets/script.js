5cb7486ce1da6c4e5e751b4a362cfa339344ea7c
BrandonSersion <addingavocado@gmail.com>
2017-10-18 15:00:51 -0400

    finished rundown

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,12 @@
   'use strict';
   angular
     .module("gemStore", [])
+    .controller("StoreCtrl", function ($http, $scope) {
+      $http.get("./assets/products.json").then(function (response) {
+        $scope.products = response.data;
+
+      })
+
+    });
 
 }());
